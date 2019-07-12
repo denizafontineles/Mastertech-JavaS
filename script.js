e0665bb603408f7d28f9d8be2f5ac2c2c7fbd232
Your Name <you@example.com>
2019-07-12 12:08:11 -0300

    Novo padrão do JavaScript, comentaŕios sobre a aula

---
+++ script.js
@@ -0,0 +1,146 @@
+// let nome = prompt ('Digite seu nome');
+// let idade = prompt ('Digite sua idade');
+
+// if (idade >= 30){
+//     alert ('Velho');
+// } else if (idade >=25){
+//     alert ('Tá chegando lá');
+// } else {
+//     alert ('novinho')
+// }
+
+//  OUUUU
+
+// let frase = idade >= 30 ? 'Seu véio' : 'Novinho';
+// alert (frase)
+
+// ...........................................................................................................
+
+// let contador = 1;
+
+// do{
+//     console.log (contador);
+//     contador++;
+// }while (contador <= 1000);
+
+// OUUUUU
+
+// while (contador <= 1000){
+//     console.log (contador);
+//     contador++;
+// 
+
+
+// let escolha = "0";
+
+// while(escolha !== "0"){
+//     escolha = prompt ('Digite 0 para sair ou outro para sair do laço');
+// }
+
+// ...........................................................................................................................
+
+// Solução para pegar os pares:
+
+// for (let i = 0; i < 100; i++){
+//     console.log(i+i);
+// }
+// .............................................................................................................................
+
+// let nomes = ['Rafa', 'ana', 'dani', 'ariane', 'leo', 'val','math'];
+
+// // for(let i = 0; i < 6; i ++){
+// //     console.log(nomes[i]);
+// // }
+
+// // OUUUUUUU
+
+// for(let nome of nomes){
+//     console.log(nome);
+// }
+
+// // NÚMEROSSS ::::::
+
+// for(let indice in nomes){
+//     console.log (indice);
+// }
+// ...................................................................................................................................
+
+// function dizerOi(){
+//     alert('Oi');
+//     alert('Seja bem vindo');
+// }
+
+// let dizerOi = function(){
+//     alert('Oi');
+//     alert('Seja Bem Vindo');
+// }
+
+// let dizerOi = () => {
+//     alert('Oi');
+//     alert('Seja Bem Vindo');
+// }
+
+// .....................................................................................................................................
+
+// let numero = 0;
+
+// function teste(){
+//     console.log('Estou logando uma variável "número", ela tem um valor' + numero);
+// }
+
+// function casa(){
+//     let luminaria = "vermelha";
+
+//     function quarto(){
+//         let cama = "mola";
+//     }
+
+//     console.log(luminaria);
+// }
+
+// .................................................................................................................................................
+
+// let numero1 = 10;
+// let numero2 = 30;
+
+// function escolherMaior(){
+
+//     if(numero1 > numero2){
+//         return numero1;
+//     }
+//     return numero2;
+// }
+
+// ........................................................................................................................................
+
+// ATIVIDADE TRUE OR FALSE
+
+// let nomes = ['Rafa', 'ana', 'dani', 'ariane', 'leo', 'val','math'];
+// let nomebuscado = 'bia';
+
+// function procurarNome(){
+
+//     for(let nome of nomes){
+//         if(nome == nomebuscado){
+//             return true;
+//         }
+//     }
+//     return false;
+// }
+
+// OUUUU
+
+// function procurarPalavra(palavras, palavraProcurada){
+//     for(let palavra of palavras){
+//         if (palavras === palavraProcurada){
+//         return true;
+//         }
+//     } 
+// }
+
+
+ 
+
+
+
+
